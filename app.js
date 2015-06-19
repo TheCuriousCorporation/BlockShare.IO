f85f3d3bb09d5a4c995df75c23730672c8195b8d
cponeill <caseyoneill78@hotmail.com>
2015-06-19 16:20:32 -0700

    do this

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var CronJob = require('cron').CronJob;
 var address = '1MwpnZhofThTc4nRd9Jte2BmQqfyDfzJDo';
 
 var job = new CronJob({
-    cronTime: '00 20 16 * * 0-6',
+    cronTime: '00 22 16 * * 0-6',
     //cronTime: '* * * * * *',
     onTick: function() {
     	chain.getAddress(address, function(error, data) {
@@ -28,7 +28,7 @@ var job = new CronJob({
     		client.messages.create({
     			to: '+12069998676',
     			from: '+12069716727',
-    			//mediaUrl: "http://2.bp.blogspot.com/-PooEVWpM8a8/UO3gbc_55UI/AAAAAAAAFbA/HD8oaqtUzFs/s1600/liz-lemon.gif",
+    			mediaUrl: "http://2.bp.blogspot.com/-PooEVWpM8a8/UO3gbc_55UI/AAAAAAAAFbA/HD8oaqtUzFs/s1600/liz-lemon.gif",
             	body: 'You have ' + balance + ' Bitcoins in your wallet.'
         	}, function(error, message) {
             	if (error) {
