91f7e4df1c5380219f515e71b025b4e6618991f7
cponeill <caseyoneill78@hotmail.com>
2015-06-21 17:53:47 -0700

    add media

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ conn.onmessage = function (ev) {
             to: '+12069998676',
             from: '+12069716727',
             body: 'You just received ' + data + ' Bitcoins!',
-            //mediaUrl: "http://i.imgur.com/63WB3ZN.gif"
+            mediaUrl: "http://i.imgur.com/63WB3ZN.gif"
         }, function(error, message) {
             if (error) {
                 console.log(error.message);
