d74e028587338a09bdcf0edb1b8666779d0fca92
Casey O'Neill <caseyoneill78@hotmail.com>
2015-11-23 21:03:39 -0800

    Update untitled.js

--- untitled.js
+++ untitled.js
@@ -48,6 +48,10 @@ var template = {
     }]
 };
 
+chain.transact(template, function(err, data) {
+  // add code here
+});
+
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
